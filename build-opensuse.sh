8bb3c0a41ad77a81009b4ba102bd40f78911952e
Coen Koomen <coen_koomen@hotmail.com>
2025-11-10 21:29:22 +0100

    Updated build script

--- build-opensuse.sh
+++ build-opensuse.sh
@@ -4,18 +4,20 @@ SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
 
 containers=(
   base
-#  iot
+  iot
   plasma
+  cosmic
 )
 
-set -e
 cd ${SCRIPTDIR}
 
 today="$(date -u +%Y%m%d)"
 
 podman pull registry.opensuse.org/opensuse/tumbleweed
+pushd . > /dev/null
 for container in ${containers[@]}; do
-  pushd opensuse-${container} > /dev/null
-  podman build . --build-arg VERSION=${today} -t localhost.localdomain/opensuse-${container}:${today}
   popd > /dev/null
+  pushd opensuse-${container} > /dev/null
+  podman build . --build-arg VERSION=${today} --cache-ttl=12h --dns=1.1.1.1 -t localhost.localdomain/opensuse-${container}:${today} || continue
+  podman tag localhost.localdomain/opensuse-${container}:${today} localhost.localdomain/opensuse-${container}:latest
 done
